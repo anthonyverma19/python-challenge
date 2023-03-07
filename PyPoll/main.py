6371087843bacbe14bec4ffbd4396f048d4d4c26
anthonyverma19 <124476768+anthonyverma19@users.noreply.github.com>
2023-03-07 13:31:51 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,1 @@
-# import os module
-import os
-# import csv module
-import csv
 
-#read in csv
-csvpath = os.path.join('Resources', 'election_data.csv')
-print(csvpath)
-#create variable for total votes cast in dataset
-#create variable for total votes cast for each of the candidates
-total_votes =[]
-stockham_votes = 0
-degette_votes = 0
-doane_votes = 0
